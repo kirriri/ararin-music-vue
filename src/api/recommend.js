307929e47a444551f5b5bf90041ac4aa313e5f05
yuggggggg <411979436@qq.com>
2019-08-11 15:36:02 +0800

    vue music recommend edited 0811

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,8 @@
 import jsonp from 'common/js/jsonp'
 import { commonParams, options } from './config'
+import axios from 'axios'
+
+// const debug = process.env.NODE_ENV !== 'production'
 
 export function getRecommend (param) {
     const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
@@ -11,3 +14,25 @@ export function getRecommend (param) {
     })
     return jsonp(url, data, options)
  }
+
+ export function getDiscList () {
+    // const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+    const url = '/api/getDiscList'
+
+    const data = Object.assign({}, commonParams, {
+      platform: 'yqq',
+      hostUin: 0,
+      sin: 0,
+      ein: 29,
+      sortId: 5,
+      needNewCode: 0,
+      categoryId: 10000000,
+      rnd: Math.random()
+    })
+
+    return axios.get(url, {
+      params: data
+    }).then((res) => {
+      return Promise.resolve(res.data)
+    })
+  }
---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,62 @@
+<template>
+    <div ref="wrapper">
+        <slot></slot>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BSroll from 'better-scroll'
+
+export default {
+    props: {
+        probeTyoe: {
+            type: Number,
+            default: 1
+        },
+        click: {
+            type: Boolean,
+            default: true
+        },
+        data: {
+            type: Array,
+            default: null
+        }
+    },
+    methods: {
+        _initScroll () {
+            if (!this.$refs.wrapper) {
+                return
+            }
+            this.scroll = new BSroll(this.$refs.wrapper, {
+                probeTyoe: this.probeTyoe,
+                click: this.click
+            })
+        },
+        enable () {
+            this.scroll && this.scroll.enable()
+        },
+        disable () {
+            this.scroll && this.scroll.disable()
+        },
+        refresh () {
+            this.scroll && this.scroll.refresh()
+        }
+    },
+    mounted () {
+        setTimeout(() => {
+            this._initScroll()
+        }, 20)
+    },
+    watch: {
+        data () {
+            setTimeout(() => {
+                this.refresh()
+            }, 20)
+        }
+    }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
