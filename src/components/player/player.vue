88c86b2294420d8f7138dbf0c8543f4835827750
yuggggggg <411979436@qq.com>
2019-08-14 08:30:32 +0800

    init player 0814

---
+++ src/components/player/player.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="player">
+        <div class="normal-player">
+            播放器
+        </div>
+        <div class="mini-player">
+
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+    
+</style>
